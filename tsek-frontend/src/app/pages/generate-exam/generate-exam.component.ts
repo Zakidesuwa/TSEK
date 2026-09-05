04160ca4df6d65c412944eca1da61b5ad2172378
Atashiwazakidaaaaaaaa <202312278@gordoncollege.edu.ph>
2026-09-05 23:03:08 +0800

    deployment issue fix

--- tsek-frontend/src/app/pages/generate-exam/generate-exam.component.ts
+++ tsek-frontend/src/app/pages/generate-exam/generate-exam.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
@@ -7,6 +7,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { ScanService } from '../../core/services/scan';
 import { driver } from 'driver.js';
+import { Subscription } from 'rxjs';
 
 type QuestionType = 'multipleChoice' | 'identification' | 'enumeration' | 'trueOrFalse';
 
